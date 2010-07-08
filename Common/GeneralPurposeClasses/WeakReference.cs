31b15a545655a644a13133ed4940ff153f997cd6
Oleksii Pokotylo <alexey.pokotylo@gmail.com>
2010-07-08 10:23:33 +0000

    создан типизированный WeakReference<> (почему не сделали такой стандартный??)

---
+++ Common/GeneralPurposeClasses/WeakReference.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace SUF.Common.GeneralPurpose
+{
+    public class WeakReference<T> where T : class
+    {
+        private WeakReference reference;
+
+        public WeakReference(T target)
+        {
+            if (target != null)
+                reference = new WeakReference(target);
+        }
+
+        private WeakReference()
+        {
+        }
+
+        public bool IsAlive
+        {
+            get
+            {
+                return reference != null && reference.IsAlive;
+            }
+        }
+
+        public T Target
+        {
+            get
+            {
+                return IsAlive ? reference.Target as T : null;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Target == null ? string.Format("WeakReference<{0}> (empty)", typeof(T).Name) : Target.ToString();
+        }
+
+        public static implicit operator WeakReference<T>(T t)
+        {
+            return t == null ? new WeakReference<T>() : new WeakReference<T>(t);
+        }
+    }
+}
