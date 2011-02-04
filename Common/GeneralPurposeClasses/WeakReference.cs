27e45d60c6c47e6dc2c0a711d9042d18f66a16ab
Bogdan Mart <mart.bogdan@gmail.com>
2011-02-04 21:05:25 +0000

    откоментировал часть старого кода..

--- Common/GeneralPurposeClasses/WeakReference.cs
+++ Common/GeneralPurposeClasses/WeakReference.cs
@@ -2,8 +2,14 @@
 
 namespace SUF.Common.GeneralPurpose
 {
+    /// <summary>
+    /// Хранит ссылку на объект, но позволяет сборщику моусора убрать объект
+    /// </summary>
     public class WeakReference<T> where T : class
     {
+        /// <summary>
+        /// Так а как?
+        /// </summary>
         private WeakReference reference;
 
         public WeakReference(T target)
@@ -34,12 +40,14 @@ namespace SUF.Common.GeneralPurpose
 
         public override string ToString()
         {
-            return Target == null ? string.Format("WeakReference<{0}> (empty)", typeof(T).Name) : Target.ToString();
+            var target = Target;
+            return target == null ? string.Format("WeakReference<{0}> (empty)", typeof(T).Name) : target.ToString();
         }
 
         public override int GetHashCode()
         {
-            return Target != null ? Target.GetHashCode() : reference.GetHashCode();
+            var target = Target;
+            return target != null ? target.GetHashCode() : reference.GetHashCode();
         }
 
         public static implicit operator WeakReference<T>(T t)
